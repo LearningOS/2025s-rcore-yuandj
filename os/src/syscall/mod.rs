74b96b75b81fae237681bf40b38147f20f785240
Dejun Yuan <yuandj1972@163.com>
2025-04-01 19:14:30 +0000

    fix: complete sys_trace and task exit

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -30,9 +30,13 @@ use process::*;
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     // 记录系统调用（必须放在最前面）
-    use crate::task::TASK_MANAGER;
-    let current = crate::task::current_task();
-    TASK_MANAGER.record_syscall(current, syscall_id);
+    //use crate::task::TASK_MANAGER;
+    //let current = crate::task::current_task();
+    //TASK_MANAGER.record_syscall(current, syscall_id);
+
+    // 先统计当前系统调用（必须放在最前面）
+    use crate::task::{current_task, TASK_MANAGER};
+    TASK_MANAGER.record_syscall(current_task(), syscall_id);
 
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
